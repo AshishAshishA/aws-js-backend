4af5499ae2199846010d3adc1af53186f788e272
AshishAshishA <ashishkr9214jmp@gmail.com>
2026-04-15 17:51:58 +0530

    task done and deployd

---
+++ backend/lambda-api-infra/test/lambda-api-infra.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as LambdaApiInfra from '../lib/lambda-api-infra-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/lambda-api-infra-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new LambdaApiInfra.LambdaApiInfraStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
